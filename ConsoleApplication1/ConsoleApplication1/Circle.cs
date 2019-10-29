3de351298e613806d8017549ff34d3a5fb2568d0
monster25101 <okthiisme@gmail.com>
2019-10-29 14:42:03 +0700

    yareyare

---
+++ ConsoleApplication1/ConsoleApplication1/Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Circle
+    {
+        private double radius;
+        private String _color;
+        public string color
+        {
+            get
+            {
+                return this._color;
+            }
+            set
+            {
+                this._color = value;
+            }
+        }
+        public Circle()
+        {
+            this.radius = 1.0;
+            this.color = "red";
+        }
+        public Circle(double r, string cl)
+        {
+            radius = r;
+            color = cl;
+        }
+        public double getRadius()
+        {
+            return radius;
+        }
+        public double getArea() {
+            return radius * radius * 3.14;
+        }
+        public string GetColor()
+        {
+            return color;
+        }
+        public void SetRadius (double newRadius)
+        {
+            radius = newRadius;
+        }
+        public String toString()
+        {
+            return "Circle[radius=" + radius + " color=" + color + "]";
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+            Console.WriteLine("The circle has radius of " + c1.getRadius() + " the area of " + c1.getArea()+" and it is " + c1.GetColor());
+            Circle c2 = new Circle(2.0, "Blue");
+            c2.SetRadius(3.2);
+            Console.WriteLine("The circle has radius of " + c2.getRadius() + " and area of " + c2.getArea() + " and it is {0} ", c2.color="yellow");
+        }
+    }
+}
